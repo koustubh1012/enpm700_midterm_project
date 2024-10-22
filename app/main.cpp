f52146e4328a9c8feff15e209ebdc775fee75c01
koustubh1012 <koustubh1012@gmail.com>
2024-10-22 19:29:14 -0400

    Task #1.08 Create application main.cpp
    In this commit we have created a main.cpp file and added the stub
    implemetation example iof humandetector class

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,38 @@
-#include "lib.hpp"
-#include "lib1.hpp"
-#include "lib2.hpp"
+#include "humandetector.hpp"
 
-int main() {
-  dummy();
-  my_function1(30);
-  bool cond = true;
-  if (cond)
-    my_function2(30.3);
-  return 0;
+int main(){
+    HumanDetector detector_object;
+
+    std::unordered_map<int, std::vector<int>> detectedhumans;
+
+    // Open video capture (0 for webcam, or replace with a video file path)
+    cv::VideoCapture cap(0, cv::CAP_V4L2);
+    if (!cap.isOpened()) {
+        std::cout << "Error: Couldn't open video capture" << std::endl;
+        return -1;
+    }
+    cv::Mat frame;
+    while (cap.read(frame)) {
+
+        detectedhumans = detector_object.detectHuman(frame);
+        for (const auto& pair : detectedhumans) {
+        std::cout << "Human ID: " << pair.first << " -> Bounding Box: ";
+        for (int coord : pair.second) {
+            std::cout << coord << " ";
+        }
+        std::cout << std::endl;
+    }
+        // Show the result
+        imshow("Human Detection", frame);
+
+        // Break if the user presses 'q'
+        if (cv::waitKey(1) == 'q') {
+            break;
+        }
+        frame.release();
+    }
+
+    cap.release();
+    cv::destroyAllWindows();
+    return 0;
 }
