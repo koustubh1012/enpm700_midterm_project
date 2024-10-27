d9d3008ed0262b2cc0e43de977334d960ca710d3
keyurborad5 <keyurborad5@gmail.com>
2024-10-27 17:22:58 -0400

    Task #2.02 Full Implementation of humandetection
    In this commit We have modified 2 files:
    1, humandetection.hpp :  we have added here a method named postprocessor which draws bounding box and label it and added class member humandetector.
    2. humandetection.cpp : We have added here the class implementation

--- include/humandetector.hpp
+++ include/humandetector.hpp
@@ -1,6 +1,6 @@
 /**
  * @file humandetector.hpp
- * @author koustubh (koustubh@umd.edu)
+ * @author koustubh (koustubh@umd.edu), Keyur Borad (kborad@umd.edu)
  * @brief Header file for Class HumanDetector
  * @version 0.1
  * @date 2024-10-22
@@ -20,7 +20,7 @@
  *
  */
 class HumanDetector {
-public:
+ public:
   /**
    * @brief Construct a new Human Detector object
    *
@@ -29,9 +29,9 @@ public:
    * @param class_names_files
    */
   HumanDetector(
-      const std::string &model_config_file = "./config/yolov3-tiny.cfg",
-      const std::string &model_weight_file = "./config/yolov3-tiny.weights",
-      const std::string &class_names_files = "./config/coco.names") {
+      const std::string& model_config_file = "./config/yolov3-tiny.cfg",
+      const std::string& model_weight_file = "./config/yolov3-tiny.weights",
+      const std::string& class_names_files = "./config/coco.names") {
     modelConfiguration = model_config_file;
     modelWeights = model_weight_file;
     classNamesFile = class_names_files;
@@ -44,6 +44,8 @@ public:
     }
     // Load the YOLO network
     net = cv::dnn::readNetFromDarknet(modelConfiguration, modelWeights);
+    net.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
+    net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
   }
 
   /**
@@ -54,11 +56,29 @@ public:
    * @return std::unordered_map<int, std::vector<int>>
    */
   std::unordered_map<int, std::vector<int>> detectHuman(cv::Mat frame);
+  /**
+   * @brief Post Processor to draw a bounding box and put text on the detected human also done non-Max suppression to avoid dublicacy detection of same human
+   * 
+   * @param frame 
+   * @param outs 
+   * @param net 
+   * @param classNames 
+   */
+  void postProcess(cv::Mat& frame, const std::vector<cv::Mat>& outs,
+                   cv::dnn::Net& net,
+                   const std::vector<std::string>& classNames);
 
-private:
+ private:
+   /*!< String Variable to load the model configuration file from the config folder */
   std::string modelConfiguration;
+   /*!< String  Variable to load the model weights  from the config folder */
   std::string modelWeights;
+   /*!< String Variable named classNamesFile to load the class names from config folder */
   std::string classNamesFile;
+   /*!< A vector of string to convert the cvs file of class names to a vector for labelling the detected object */
   std::vector<std::string> classNames;
+   /*!< OPENCV class of defining DNN neuaral net for parsing the frame */
   cv::dnn::Net net;
+   /*!< unordered_map of integer and Vactor that is sent as a return of the detected human class*/
+  std::unordered_map<int, std::vector<int>> detectedhumans;
 };
