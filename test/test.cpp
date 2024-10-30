92d4a880cdba087e9fe8d1981d6c3cd47f5adb42
keyurborad5 <keyurborad5@gmail.com>
2024-10-30 00:03:03 -0400

    Task #1.09 Create Unit tests
    In this commit we have added unit test cases for human poseestimator class

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
-#include "humandetector.hpp"
+#include <vector>
+#include "humanposeestimator.hpp"
 
 TEST(HUMAN_DETECTION, NO_HUMAN_TEST) {
   HumanDetector test_object;
@@ -23,4 +24,25 @@ TEST(HUMAN_DETECTION, TWO_HUMAN_TEST) {
   std::unordered_map<int, std::vector<int>> detectedhumans;
   detectedhumans = test_object.detectHuman(frame);
   ASSERT_EQ(detectedhumans.size(), 2);
+}
+
+TEST(HUMAN_POSE_ESTIMATION, NO_HUMAN_TEST) {
+  HumanPoseEstimator test_object;
+  cv::Mat frame = imread("test/no_human.jpg", cv::IMREAD_COLOR);
+  std::unordered_map<int, std::vector<float>> human_poses = test_object.poseEstimator(frame);
+  ASSERT_TRUE(human_poses.empty());
+}
+
+TEST(HUMAN_POSE_ESTIMATION, ONE_HUMAN_TEST) {
+  HumanPoseEstimator test_object;
+  cv::Mat frame = imread("test/human.jpg", cv::IMREAD_COLOR);
+  std::unordered_map<int, std::vector<float>> human_poses = test_object.poseEstimator(frame);
+  ASSERT_EQ(human_poses.size(), 1);
+}
+
+TEST(HUMAN_POSE_ESTIMATION, TWO_HUMAN_TEST) {
+  HumanPoseEstimator test_object;
+  cv::Mat frame = imread("test/two_human.jpg", cv::IMREAD_COLOR);
+  std::unordered_map<int, std::vector<float>> human_poses = test_object.poseEstimator(frame);
+  ASSERT_EQ(human_poses.size(), 2);
 }
